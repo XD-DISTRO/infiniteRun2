6e600a16b2fb9edb5fb4452b6e7e424ed0328b52
XD-DISTRO <74779316+XD-DISTRO@users.noreply.github.com>
2021-05-22 10:03:58 +0530

    Add files via upload

---
+++ AngryBirdsStage4-master/sketch.js
@@ -0,0 +1,50 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var box1, pig1;
+var backgroundImg,platform;
+var bird, slingShot;
+
+function preload() {
+    backgroundImg = loadImage("sprites/bg.png");
+}
+
+function setup(){
+    var canvas = createCanvas(displayWidth - 20, displayHeight-30);
+    engine = Engine.create();
+    world = engine.world;
+
+
+    ground = new Ground(600,height,1200,20);
+    ground2 = new Ground(200, 260, 1200, 20);
+   
+    bird = new Bird(20, 20, 100, 100)
+    //log6 = new Log(230,180,80, PI/2);
+    slingshot = new SlingShot(bird.body,{x:200, y:50});
+}
+
+function draw(){
+    background(backgroundImg);
+    Engine.update(engine);
+    strokeWeight(4);
+   
+    ground.display();
+    ground2.display();
+
+    
+  
+    //log6.display();
+    slingshot.display();    
+}
+
+function mouseDragged(){
+    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+}
+
+
+function mouseReleased(){
+    slingshot.fly();
+}
